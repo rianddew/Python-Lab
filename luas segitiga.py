fb6aba4ea8155b6753bcf4289a2ce1693a5775d2
Rianda Rahma Dewanty <120345499+rianddew@users.noreply.github.com>
2026-03-25 23:39:41 +0700

    Add files via upload

---
+++ luas segitiga.py
@@ -0,0 +1,4 @@
+alas = int(input("Masukkan alas: "))
+tinggi = int(input("Masukkan tinggi: "))
+luas = alas * tinggi / 2
+print(f"Luas segitiga dengan panjang alas {alas} dan tinggi {tinggi} adalah {luas}")
